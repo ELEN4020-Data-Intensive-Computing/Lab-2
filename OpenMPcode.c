2255e3f624d37eb946dfe0cba7a84e3ed2a61638
Sizwe Lekoba <lekobas@DLAB52.EIEUG>
2018-03-05 21:58:17 +0200

    OpenMP solution

---
+++ OpenMPcode.c
@@ -0,0 +1,87 @@
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+void PrintTranspose(int **arraY, int SquareSize) {
+
+int N = SquareSize;
+
+	for(int j = 0; j < N; j++) {
+  		for(int k = 0; k < N; k++) {
+			printf("%d ", arraY[j][k]);
+		}
+		printf("\n");
+	}
+}
+
+void Transpose(int **arraY, int SquareSize) {
+	int nthreads, tid, i, j, k, chunk=SquareSize, count, starterRow=0, starterCol=0;
+	int N = SquareSize;
+ 	//int * a = arraY;
+	omp_set_num_threads(8);
+
+	double start = omp_get_wtime();
+		#pragma omp parallel shared(arraY,nthreads,chunk,starterRow,starterCol,i,j,k) private(tid) 
+		  {
+		 	 tid = omp_get_thread_num();
+		 	 if (tid == 0) //thread 0 belongs to master thread
+		    	{
+		    		nthreads = omp_get_num_threads();
+		   		//printf("Number of threads = %d\n", nthreads);
+		   	 }
+		 // printf("Thread %d starting...\n",tid);
+	
+	#pragma omp parallel for schedule(dynamic,chunk)
+	for(int i = starterRow; i < N+1; i++){
+	 // #pragma omp parallel for
+		if(i > 0) {
+			starterCol++;		
+		}
+	  for (int j=starterCol; j<N; j++)
+	    {
+		
+		 int temp = arraY[i][j];
+	 	arraY[i][j] = arraY[j][i];
+		 arraY[j][i] = temp;
+	    }
+	 }
+		  
+	}
+
+	double time  = omp_get_wtime() - start;
+	printf("Time in seconds = %f",time );	
+	printf("\n");
+	PrintTranspose(arraY,SquareSize);
+}
+
+int main(){
+
+	int **a;
+	int rows = 128;
+	int cols = 128;
+	int i;
+	int temp = i + 1; 
+	a = malloc(rows * sizeof (int *));
+
+	 // for each row allocate cols int
+  	for (int i = 0; i < rows; i++) {
+        	a[i] = malloc(cols * sizeof (int));
+    	}
+	
+     //populating the elements of the array
+     for ( i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+           int temp = i*rows + j; 
+           a[i][j] = temp;
+        }
+    }
+
+	Transpose(a,rows);
+
+}
+
